9df7be0771be1132f9902765dbe9359728684558
nahuel <lescanoagustin10nahuel@gmail.com>
2023-06-10 03:01:59 -0300

    Adding some validations.

--- src/models/Dog.js
+++ src/models/Dog.js
@@ -13,26 +13,43 @@ module.exports = (sequelize) => {
       },
       image: {
         type: DataTypes.STRING,
-        defaultValue: 'https://cdn2.thedogapi.com/images/rkiByec47.jpg',
+        defaultValue:
+          'https://www.cnet.com/a/img/resize/20d6844768bd3f5f0df41deee97897423bcaf3c5/hub/2021/11/03/3c2a7d79-770e-4cfa-9847-66b3901fb5d7/c09.jpg?auto=webp&fit=crop&height=1200&width=1200',
         validate: {
+          is: /^https?:\/\/.*\/.*\.(png|gif|webp|jpeg|jpg)\??.*$/i,
           isUrl: true,
         },
       },
       name: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          is: /^([a-zA-Z ]+)$/,
+          notNull: { msg: 'Cannot be null' },
+          notEmpty: true,
+        },
       },
       height: {
         type: DataTypes.JSON,
         allowNull: false,
+        validate: {
+          notNull: { msg: 'Cannot be null' },
+        },
       },
       weight: {
         type: DataTypes.JSON,
         allowNull: false,
+        validate: {
+          notNull: { msg: 'Cannot be null' },
+        },
       },
       life_span: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          is: /^([a-zA-Z0-9 -_ ]+)$/,
+          notNull: { msg: 'Cannot be null' },
+        },
       },
     },
     {
