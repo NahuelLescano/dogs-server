50183137a5bb42072968767417ca074f6fffb674
nahuel <lescanoagustin10nahuel@gmail.com>
2023-06-11 23:01:03 -0300

    Now temperament is created once.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const server = require('./src/app.js');
+// const {
+//   getTemperaments,
+// } = require('./src/controllers/temperamentController.js');
 const { conn } = require('./src/db.js');
 const { PORT } = process.env;
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
+  // getTemperaments();
   console.log('Connecting to the database');
   server.listen(PORT, () => {
     console.log(`Listening at port: ${PORT}`);
